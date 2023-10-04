245f0fdeabee54ca4d5a3a785e8df2887903bb2c
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-10-04 15:07:10 +0200

    fix: assets path

--- views/templates/base.php
+++ views/templates/base.php
@@ -6,7 +6,7 @@
     <title>Exercise Looper Clone</title>
     <script src="https://kit.fontawesome.com/f5c1a22e81.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css">
-    <link rel="stylesheet" href="build/app.css">
+    <link rel="stylesheet" href="/build/app.css">
 </head>
 <body>
 <?=$content?>
