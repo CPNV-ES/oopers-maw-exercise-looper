6bbdb1bf28c654d7e6f8ba55eee061b12af5b125
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-10-04 15:18:11 +0200

    fix: fixed views list indentation

--- views/exercises/answering/list.php
+++ views/exercises/answering/list.php
@@ -4,34 +4,37 @@
         <span class="title-label">Title: <span class="title-label-bold">a</span></span>
     </section>
 </header>
-<div class="answering-list">
-    <div class="column card">
-        <div class="title">Linux base commands</div>
-        <a class="button" href="/exercises/9/fulfillments/new">Take it</a>
-    </div>
 
-    <div class="column card">
-        <div class="title">CLD2 Cloud Services</div>
-        <a class="button" href="/exercises/12/fulfillments/new">Take it</a>
-    </div>
+<main class="container">
+    <div class="answering-list">
+        <div class="column card">
+            <div class="title">Linux base commands</div>
+            <a class="button" href="/exercises/9/fulfillments/new">Take it</a>
+        </div>
 
-    <div class="column card">
-        <div class="title">Vidéo de Sylvain</div>
-        <a class="button" href="/exercises/22/fulfillments/new">Take it</a>
-    </div>
+        <div class="column card">
+            <div class="title">CLD2 Cloud Services</div>
+            <a class="button" href="/exercises/12/fulfillments/new">Take it</a>
+        </div>
 
-    <div class="column card">
-        <div class="title">Vidéo d'Amos</div>
-        <a class="button" href="/exercises/29/fulfillments/new">Take it</a>
-    </div>
+        <div class="column card">
+            <div class="title">Vidéo de Sylvain</div>
+            <a class="button" href="/exercises/22/fulfillments/new">Take it</a>
+        </div>
 
-    <div class="column card">
-        <div class="title">JS assessment</div>
-        <a class="button" href="/exercises/30/fulfillments/new">Take it</a>
-    </div>
+        <div class="column card">
+            <div class="title">Vidéo d'Amos</div>
+            <a class="button" href="/exercises/29/fulfillments/new">Take it</a>
+        </div>
 
-    <div class="column card">
-        <div class="title">SchoolManagement V1</div>
-        <a class="button" href="/exercises/31/fulfillments/new">Take it</a>
+        <div class="column card">
+            <div class="title">JS assessment</div>
+            <a class="button" href="/exercises/30/fulfillments/new">Take it</a>
+        </div>
+
+        <div class="column card">
+            <div class="title">SchoolManagement V1</div>
+            <a class="button" href="/exercises/31/fulfillments/new">Take it</a>
+        </div>
     </div>
-</div>
+</main>
