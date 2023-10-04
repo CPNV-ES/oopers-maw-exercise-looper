cafe873e01c073a491c9ea7b21aba4229436520d
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-10-04 15:17:44 +0200

    feat : exercices management list

---
+++ src/Controller/ExercisesManagement.php
@@ -0,0 +1,15 @@
+<?php
+
+use MVC\Http\Controller\Controller;
+use MVC\Http\Routing\Annotation\Route;
+use \MVC\Http\Response\Response;
+
+#[Route("/exercises")]
+class ExercisesManagement extends Controller
+{
+    #[Route("/")]
+    function showExercisesList(): Response
+    {
+        return $this->render('exercises/management/list');
+    }
+}
