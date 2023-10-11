511d957c1e61fa5c05e121c0f0a362eac072cfb1
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-10-11 10:41:52 +0200

    feat: added two results views

---
+++ views/exercises/management/results-by-fulfillment.php
@@ -0,0 +1,12 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/../views/headers/heading-results.php" ?>
+
+<main class="container">
+    <h1>2023-09-05 14:38:00 UTC</h1>
+    <dl class="answer">
+        <dt>Décriver le polymorphisme</dt>
+        <dd>C'est en relation avec la POO</dd>
+        <dt>L'héritage c'est bien?</dt>
+        <dd>rthtrht</dd>
+    </dl>
+</main>
