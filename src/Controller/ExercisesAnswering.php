f1c1106689f8211767980925cf906ca7be2f0755
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-10-11 10:13:25 +0200

    feat: added fulfillments edition controller+view

--- src/Controller/ExercisesAnswering.php
+++ src/Controller/ExercisesAnswering.php
@@ -1,6 +1,7 @@
 <?php
 
 use MVC\Http\Controller\Controller;
+use MVC\Http\HTTPMethod;
 use MVC\Http\Routing\Annotation\Route;
 use \MVC\Http\Response\Response;
 
@@ -13,9 +14,28 @@ class ExercisesAnswering extends Controller
     {
         return $this->render('exercises/answering/list');
     }
-    #[Route("/[:id]/fulfillments/new")]
-    function showNewFulfillment(): Response
+    #[Route("/[:exerciceid]/fulfillments/new")]
+    function showNewFulfillment($exerciceid): Response
     {
         return $this->render('exercises/answering/fulfillment');
+    }
+    #[Route("/[:exerciceid]/fulfillments",methods: [HTTPMethod::POST])]
+    function addFulfillment($exerciceid): Response
+    {
+        $response = new Response();
+        $response->headers->set('Location', "fulfillments/1/edit");
+        return $response;
+    }
+    #[Route("/[:exerciceid]/fulfillments/[:fulfillmentid]/edit")]
+    function showEditFulfillment($exerciceid,$fulfillmentid): Response
+    {
+        return $this->render('exercises/answering/fulfillmentEdit');
+    }
+    #[Route("/[:exerciceid]/fulfillments/[:fulfillmentid]",methods: [HTTPMethod::POST])]
+    function editFulfillment($exerciceid,$fulfillmentid): Response
+    {
+        $response = new Response();
+        $response->headers->set('Location', "$fulfillmentid/edit");
+        return $response;
     }
 }
