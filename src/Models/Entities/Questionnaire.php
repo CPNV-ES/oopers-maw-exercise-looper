cbce51ab90abb558c465f0913bfae144318c93eb
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-11-15 08:24:46 +0100

    feat: added first version of Questionnaire Entity

---
+++ src/Models/Entities/Questionnaire.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models\Entities;
+
+use ORM\Column;
+use ORM\Table;
+
+#[Table("questionnaires")]
+class Questionnaire
+{
+    #[Column("id")]
+    private int $id;
+    #[Column("title")]
+    private string $title;
+    #[Column("questionnaire_state_id")]
+    private QuestionnaireState $state;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getState(): QuestionnaireState
+    {
+        return $this->state;
+    }
+
+    public function setState(QuestionnaireState $state): void
+    {
+        $this->state = $state;
+    }
+}
