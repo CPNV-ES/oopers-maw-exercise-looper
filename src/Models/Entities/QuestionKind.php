caae6c7b81e10eaffaa1dd030169365ab263c8a4
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-11-15 08:58:30 +0100

    Using enums for simple states instead of tables

--- src/Models/Entities/QuestionKind.php
+++ src/Models/Entities/QuestionKind.php
@@ -2,35 +2,9 @@
 
 namespace App\Models\Entities;
 
-use ORM\Column;
-use ORM\Table;
-
-#[Table("question_kind")]
-class QuestionKind
+enum QuestionKind
 {
-    #[Column("id")]
-    private int $id;
-    //TODO : Fix the name in DB
-    #[Column("displayed name")]
-    private int $displayedName;
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-
-    public function getDisplayedName(): int
-    {
-        return $this->displayedName;
-    }
-
-    public function setDisplayedName(int $displayedName): void
-    {
-        $this->displayedName = $displayedName;
-    }
+    case SingeLineText;
+    case ListOfSingleLines;
+    case MultilineText;
 }
