fedb2c9ddbc4b9b963804db9f59d2c32819f94d7
dimitri-rutz <dimitri@drutz.ch>
2023-11-18 18:49:04 +0100

    feat: create Filling entity

---
+++ src/Entity/Filling.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use ORM as ORM;
+
+#[ORM\Table()]
+class Filling
+{
+
+    #[ORM\Column('id')]
+    private int $id;
+
+    #[ORM\Column('questionnaires_id')]
+    private int|Exercise $exercise;
+
+    #[ORM\Column('submission_date')]
+    private \DateTimeImmutable $submission_date;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Filling
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getExercise(): Exercise|int
+    {
+        return $this->exercise;
+    }
+
+    public function setExercise(Exercise|int $exercise): Filling
+    {
+        $this->exercise = $exercise;
+        return $this;
+    }
+
+    public function getSubmissionDate(): \DateTimeImmutable
+    {
+        return $this->submission_date;
+    }
+
+    public function setSubmissionDate(\DateTimeImmutable $submission_date): Filling
+    {
+        $this->submission_date = $submission_date;
+        return $this;
+    }
+
+
+}
