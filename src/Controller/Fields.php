74d549c212c020ff1f0fe4e9a4491bba1e4c6e65
dimitri-rutz <dimitri@drutz.ch>
2023-11-20 08:35:55 +0100

    feat: change param name

--- src/Controller/Fields.php
+++ src/Controller/Fields.php
@@ -8,36 +8,36 @@ use MVC\Http\Response\Response;
 use MVC\Http\Routing\Annotation\Route;
 
 
-#[Route("/exercises/[:exerciceId]/fields", name:"exercises.fields.")]
+#[Route("/exercises/[:e_id]/fields", name:"exercises.fields.")]
 class Fields extends Controller
 {
-    #[Route("/", name: "show")]
-    public function showExerciseFields(int $exerciceId): Response
+    #[Route("", name: "show")]
+    public function show(int $e_id): Response
     {
-        return $this->render('exercises.creation.fields',["exerciceId"=>$exerciceId]);
+        return $this->render('exercises.creation.fields',["exerciceId"=>$e_id]);
     }
 
     #[Route("/", name: "create", methods: [HTTPMethod::POST])]
-    public function createExerciseField(int $exerciceId): Response
+    public function createExerciseField(int $e_id): Response
     {
-        return $this->render('exercises.creation.fields',["exerciceId"=>$exerciceId]);
+        return $this->render('exercises.creation.fields',["exerciceId"=>$e_id]);
     }
 
     #[Route("/[:fieldId]/edit", name: 'edit')]
-    public function showFieldEdition(int $exerciceId, int $fieldId): Response
+    public function showFieldEdition(int $e_id, int $fieldId): Response
     {
-        return $this->render('exercises.creation.field-edit',["exerciceId"=>$exerciceId,"fieldId"=>$fieldId]);
+        return $this->render('exercises.creation.field-edit',["exerciceId"=>$e_id,"fieldId"=>$fieldId]);
     }
 
     #[Route("[:fieldId]", name: 'update', methods: [HTTPMethod::POST])]
-    public function editField(int $exerciceId, int $fieldId): Response
+    public function editField(int $e_id, int $fieldId): Response
     {
-        return $this->redirectToRoute("exercises.fields.list",["exerciceId"=>$exerciceId]);
+        return $this->redirectToRoute("exercises.fields.list",["exerciceId"=>$e_id]);
     }
 
     #[Route("[:fieldId]", name: 'delete', methods: [HTTPMethod::DELETE])]
-    public function deleteField(int $exerciceId, int $fieldId): Response
+    public function deleteField(int $e_id, int $fieldId): Response
     {
-        return $this->redirectToRoute("exercises.fields.list",["exerciceId"=>$exerciceId,"fieldId"=>$fieldId]);
+        return $this->redirectToRoute("exercises.fields.list",["exerciceId"=>$e_id,"fieldId"=>$fieldId]);
     }
 }
