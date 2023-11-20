0d641af5fe351a050cd70544607e94fe30c77aa5
dimitri-rutz <dimitri@drutz.ch>
2023-11-20 08:36:16 +0100

    feat: update to match params names

--- views/exercises/creation/fields.php
+++ views/exercises/creation/fields.php
@@ -18,22 +18,22 @@
                     <td>A</td>
                     <td>single_line</td>
                     <td>
-                        <a title="Edit" href="<?=$this->url("exercises.fields.edit",["exerciceId"=>$this->exerciceId,"fieldId"=>1])?>"><i class="fa fa-edit"></i></a>
+                        <a title="Edit" href="<?=$this->url("exercises.fields.edit",["e_id"=>$this->exerciceId,"fieldId"=>1])?>"><i class="fa fa-edit"></i></a>
                         <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete"
-                           href="<?=$this->url("exercises.fields.delete",["exerciceId"=>$this->exerciceId,"fieldId"=>1])?>"><i class="fa fa-trash"></i></a>
+                           href="<?=$this->url("exercises.fields.delete",["e_id"=>$this->exerciceId,"fieldId"=>1])?>"><i class="fa fa-trash"></i></a>
                     </td>
                 </tr>
                 </tbody>
             </table>
 
             <a data-confirm="Are you sure? You won't be able to further edit this exercise" class="button"
-               rel="nofollow" data-method="put" href="<?=$this->url("exercises.update",["exerciceId"=>$this->exerciceId])?>?exercise%5Bstatus%5D=answering"><i
+               rel="nofollow" data-method="put" href="<?=$this->url("exercises.update",["id"=>$this->exerciceId])?>?exercise%5Bstatus%5D=answering"><i
                         class="fa fa-comment"></i> Complete and be ready for answers</a>
 
         </section>
         <section class="column">
             <h1>New Field</h1>
-            <form action="<?=$this->url("exercises.fields.create",["exerciceId"=>$this->exerciceId])?>" accept-charset="UTF-8" method="post">
+            <form action="<?=$this->url("exercises.fields.create",["e_id"=>$this->exerciceId])?>" accept-charset="UTF-8" method="post">
 
                 <div class="field">
                     <label for="field_label">Label</label>
