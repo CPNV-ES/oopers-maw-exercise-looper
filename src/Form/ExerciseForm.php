a00599681879677a7203c1f8e7d52dd151023d4b
dimitri-rutz <dimitri@drutz.ch>
2023-11-20 08:39:58 +0100

    feat: add Exercise form

---
+++ src/Form/ExerciseForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use MVC\Form\AbstractForm;
+use MVC\Form\Field\TextField;
+
+class ExerciseForm extends AbstractForm
+{
+
+    public function __construct(object $entity)
+    {
+        parent::__construct($entity);
+        $this->addOption('view_template', 'exercises/creation/new-form');
+        $this->buildForm();
+    }
+
+    public function buildForm(): void
+    {
+        $this->add('title', TextField::class, [
+            'label' => 'Title',
+            'attributes' => [
+                'class' => ['field']
+            ]
+        ]);
+    }
+}
