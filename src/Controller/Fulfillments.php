2ec351ef4216979c34e184eadb798e15c1a2df98
dimitri-rutz <dimitri@drutz.ch>
2023-11-24 09:34:45 +0100

    fix: typo 'exercice' => 'exercise'

--- src/Controller/Fulfillments.php
+++ src/Controller/Fulfillments.php
@@ -7,25 +7,25 @@ use MVC\Http\HTTPMethod;
 use MVC\Http\Response\Response;
 use MVC\Http\Routing\Annotation\Route;
 
-#[Route("/exercises/[:exerciceId]/fulfillments", name:"exercises.fulfillments.")]
+#[Route("/exercises/[:exerciseId]/fulfillments", name:"exercises.fulfillments.")]
 class Fulfillments extends Controller
 {
 
-    #[Route("/new", name: 'new')]
-    public function showNewFulfillment(int $exerciceId): Response
+    #[Route("/new", name: 'new', methods: [HTTPMethod::GET, HTTPMethod::POST])]
+    public function new(int $exerciseId, SQLOperations $operations): Response
     {
         return $this->render('exercises.answering.fulfillment',["exerciceId"=>$exerciceId]);
     }
 
     #[Route("/", name: 'create', methods: [HTTPMethod::POST])]
-    public function addFulfillment(int $exerciceId): Response
+    public function addFulfillment(int $exerciseId): Response
     {
         $fulfillmentId = 0;
-        return $this->redirectToRoute("exercises.fulfillments.edit",["exerciceId"=>$exerciceId,"fulfillmentId"=>$fulfillmentId]);
+        return $this->redirectToRoute("exercises.fulfillments.edit",["exerciceId"=>$exerciseId,"fulfillmentId"=>$fulfillmentId]);
     }
 
     #[Route("/[:fulfillmentId]/edit", name: 'edit')]
-    public function showEditFulfillment(int $exerciceId, int $fulfillmentId): Response
+    public function showEditFulfillment(int $exerciseId, int $fulfillmentId): Response
     {
         return $this->render('exercises.answering.fulfillmentEdit',["exerciceId"=>$exerciceId,"fulfillmentId"=>$fulfillmentId]);
     }
